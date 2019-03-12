5e4544de56daee0a868cf983b702932f34ecc289
Nikolay <kovalenko.mykola@gmail.com>
2019-03-12 21:37:56 +0200

    First angular page with API integration

--- PublicApi/Startup.cs
+++ PublicApi/Startup.cs
@@ -41,6 +41,8 @@ namespace PublicApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder =>
+       builder.WithOrigins("http://localhost:4200"));
             app.UseAuthentication();
 
             if (env.IsDevelopment())
---
+++ Web/ZHWeb/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ApiComponent } from './api/api.component';
+import { ApiService } from './api.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ApiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [ApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
