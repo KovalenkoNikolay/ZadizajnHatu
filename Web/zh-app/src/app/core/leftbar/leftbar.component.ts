b7e6ba91c6542d3f35d34672b988eb0119db164e
Nikolay Kovalenko <nikolay.kovalenko@betssongroup.com>
2019-06-13 17:54:30 +0300

    Implement switching filter\details

--- Web/zh-app/src/app/core/leftbar/leftbar.component.ts
+++ Web/zh-app/src/app/core/leftbar/leftbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PageStateService } from '../services/page-state.service';
 
 @Component({
+  providers: [PageStateService],
   selector: '[id=leftbar]',
   templateUrl: './leftbar.component.html',
   styleUrls: ['./leftbar.component.scss']
 })
 export class LeftbarComponent implements OnInit {
-
-  constructor() { }
+  constructor(public pageStateService :PageStateService) { }
 
   ngOnInit() {
   }
 
+  public GetPageState() : string {
+    return this.pageStateService.getCurrentPageName();
+  } 
 }
--- Web/zh-app/src/app/modules/home/pages/app.component.ts
+++ Web/zh-app/src/app/modules/home/pages/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { PageStateService } from '../../../core/services/page-state.service';
 
 @Component({
-  providers: [PageStateService],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
   title = 'zh-app';
+
 }
