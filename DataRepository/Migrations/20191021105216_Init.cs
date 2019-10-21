eb3394c9598cb796d58e357c11062c5073d949fd
Nikolay Kovalenko <nikolay.kovalenko@betssongroup.com>
2019-10-21 13:56:57 +0300

    Fix Migration

---
+++ DataRepository/Migrations/20191021105216_Init.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataRepository.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    CountryId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.CountryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkerType",
+                columns: table => new
+                {
+                    WorkerTypeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkerType", x => x.WorkerTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Region",
+                columns: table => new
+                {
+                    RegionId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    CountryId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Region", x => x.RegionId);
+                    table.ForeignKey(
+                        name: "FK_Region_Country_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Country",
+                        principalColumn: "CountryId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Service",
+                columns: table => new
+                {
+                    ServiceId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    WorkerTypeId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Service", x => x.ServiceId);
+                    table.ForeignKey(
+                        name: "FK_Service_WorkerType_WorkerTypeId",
+                        column: x => x.WorkerTypeId,
+                        principalTable: "WorkerType",
+                        principalColumn: "WorkerTypeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    WorkerId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    ContactInfo = table.Column<string>(nullable: true),
+                    WorkerTypeId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.WorkerId);
+                    table.ForeignKey(
+                        name: "FK_Users_WorkerType_WorkerTypeId",
+                        column: x => x.WorkerTypeId,
+                        principalTable: "WorkerType",
+                        principalColumn: "WorkerTypeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    CityId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    RegionId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.CityId);
+                    table.ForeignKey(
+                        name: "FK_City_Region_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Region",
+                        principalColumn: "RegionId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PortfolioProject",
+                columns: table => new
+                {
+                    PortfolioProjectId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    WorkerId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PortfolioProject", x => x.PortfolioProjectId);
+                    table.ForeignKey(
+                        name: "FK_PortfolioProject_Users_WorkerId",
+                        column: x => x.WorkerId,
+                        principalTable: "Users",
+                        principalColumn: "WorkerId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkerPrice",
+                columns: table => new
+                {
+                    WorkerPriceId = table.Column<Guid>(nullable: false),
+                    Amount = table.Column<decimal>(nullable: false),
+                    WorkerId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkerPrice", x => x.WorkerPriceId);
+                    table.ForeignKey(
+                        name: "FK_WorkerPrice_Users_WorkerId",
+                        column: x => x.WorkerId,
+                        principalTable: "Users",
+                        principalColumn: "WorkerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkerService",
+                columns: table => new
+                {
+                    WorkerServiceId = table.Column<Guid>(nullable: false),
+                    ServiceId = table.Column<int>(nullable: false),
+                    WorkerId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkerService", x => x.WorkerServiceId);
+                    table.ForeignKey(
+                        name: "FK_WorkerService_Service_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Service",
+                        principalColumn: "ServiceId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkerService_Users_WorkerId",
+                        column: x => x.WorkerId,
+                        principalTable: "Users",
+                        principalColumn: "WorkerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkerCity",
+                columns: table => new
+                {
+                    WorkerCityId = table.Column<Guid>(nullable: false),
+                    CityId = table.Column<Guid>(nullable: false),
+                    WorkerId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkerCity", x => x.WorkerCityId);
+                    table.ForeignKey(
+                        name: "FK_WorkerCity_City_CityId",
+                        column: x => x.CityId,
+                        principalTable: "City",
+                        principalColumn: "CityId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkerCity_Users_WorkerId",
+                        column: x => x.WorkerId,
+                        principalTable: "Users",
+                        principalColumn: "WorkerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkGeography",
+                columns: table => new
+                {
+                    WorkGeographyId = table.Column<Guid>(nullable: false),
+                    CityId = table.Column<Guid>(nullable: true),
+                    RegionId = table.Column<Guid>(nullable: true),
+                    CountryId = table.Column<Guid>(nullable: true),
+                    WorkerId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkGeography", x => x.WorkGeographyId);
+                    table.ForeignKey(
+                        name: "FK_WorkGeography_City_CityId",
+                        column: x => x.CityId,
+                        principalTable: "City",
+                        principalColumn: "CityId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_WorkGeography_Country_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Country",
+                        principalColumn: "CountryId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_WorkGeography_Region_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Region",
+                        principalColumn: "RegionId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_WorkGeography_Users_WorkerId",
+                        column: x => x.WorkerId,
+                        principalTable: "Users",
+                        principalColumn: "WorkerId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    ImageId = table.Column<Guid>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    PortfolioProjectId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.ImageId);
+                    table.ForeignKey(
+                        name: "FK_Image_PortfolioProject_PortfolioProjectId",
+                        column: x => x.PortfolioProjectId,
+                        principalTable: "PortfolioProject",
+                        principalColumn: "PortfolioProjectId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_RegionId",
+                table: "City",
+                column: "RegionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_PortfolioProjectId",
+                table: "Image",
+                column: "PortfolioProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PortfolioProject_WorkerId",
+                table: "PortfolioProject",
+                column: "WorkerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Region_CountryId",
+                table: "Region",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Service_WorkerTypeId",
+                table: "Service",
+                column: "WorkerTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_WorkerTypeId",
+                table: "Users",
+                column: "WorkerTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkerCity_CityId",
+                table: "WorkerCity",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkerCity_WorkerId",
+                table: "WorkerCity",
+                column: "WorkerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkerPrice_WorkerId",
+                table: "WorkerPrice",
+                column: "WorkerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkerService_ServiceId",
+                table: "WorkerService",
+                column: "ServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkerService_WorkerId",
+                table: "WorkerService",
+                column: "WorkerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkGeography_CityId",
+                table: "WorkGeography",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkGeography_CountryId",
+                table: "WorkGeography",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkGeography_RegionId",
+                table: "WorkGeography",
+                column: "RegionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkGeography_WorkerId",
+                table: "WorkGeography",
+                column: "WorkerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+
+            migrationBuilder.DropTable(
+                name: "WorkerCity");
+
+            migrationBuilder.DropTable(
+                name: "WorkerPrice");
+
+            migrationBuilder.DropTable(
+                name: "WorkerService");
+
+            migrationBuilder.DropTable(
+                name: "WorkGeography");
+
+            migrationBuilder.DropTable(
+                name: "PortfolioProject");
+
+            migrationBuilder.DropTable(
+                name: "Service");
+
+            migrationBuilder.DropTable(
+                name: "City");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Region");
+
+            migrationBuilder.DropTable(
+                name: "WorkerType");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+        }
+    }
+}
